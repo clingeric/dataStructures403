9f9237740d1f6dada830b869a56b14b9d8899771
Tyler Bradshaw <bradshaw.tyler@gmail.com>
2017-10-02 11:40:58 -0600

    Added menu option #1 text

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -24,6 +24,11 @@ namespace ConsoleApp1
                 case 1:
                     Console.WriteLine("1.Add one time to Stack");
                     Console.WriteLine("2. Add Huge List of Items to Stack");
+                    Console.WriteLine("3. Display Stack");
+                    Console.WriteLine("4. Delete from Stack");
+                    Console.WriteLine("5. Clear Stack");
+                    Console.WriteLine("6. Search Stack");
+                    Console.WriteLine("7. Return to Main Menu");
                     break;
                 case 2:
                     break;
