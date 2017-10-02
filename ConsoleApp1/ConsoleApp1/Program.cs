70b61e2a9baae4b1b7c7f10a6b2d5e6c9d13321c
Eric Clinger <cling.eric@gmail.com>
2017-10-02 16:44:08 -0600

    Finished add, addHuge, and display functions

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,7 +8,113 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
+
+        //STACK
+
+        static Stack<string> myStack = new Stack<string>();
+        
+        static void addToStack()
+        {
+            //prompts the user to enter a string and then inserts the input into the data structure.
+            Console.WriteLine("Enter a string");
+            string userInput = Console.ReadLine();
+            myStack.Push(userInput);
+        }
+        static void addHugeToStack()
+        {
+            //first clears the data structure and 
+            //then generate 2,000 items in the data structure with the value of “New Entry” concatenated with the number. 
+            //For example, New Entry 1, New Entry 2, New Entry 3. For the dictionary, the key will be the generated string ("New Entry 2") and the value will be the current number (2).
+            myStack.Clear();
+            for (int i = 1; i < 2001; i++)
+            { 
+                myStack.Push("New Entry " + i);
+            }
+        }
+        static void displayStack()
+        {
+            //display the contents of the data structure.You must use the foreach loop when displaying the data. Handle any errors and inform the user.
+            foreach (string item in myStack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+
+
+        //QUEUE
+
+        static Queue<string> myQueue = new Queue<string>();
+        
+        static void addToQueue()
+        {
+            //prompts the user to enter a string and then inserts the input into the data structure.
+            Console.WriteLine("Enter a string");
+            string userInput = Console.ReadLine();
+            myQueue.Enqueue(userInput);
+        }
+        static void addHugeToQueue()
+        {
+            //first clears the data structure and 
+            //then generate 2,000 items in the data structure with the value of “New Entry” concatenated with the number. 
+            //For example, New Entry 1, New Entry 2, New Entry 3. For the dictionary, the key will be the generated string ("New Entry 2") and the value will be the current number (2).
+            myQueue.Clear();
+            for (int i = 0; i < 2001; i++)
+            {
+                myQueue.Enqueue("New Entry " + i);
+            }
+        }
+        static void displayQueue()
+        {
+            //display the contents of the data structure.You must use the foreach loop when displaying the data. Handle any errors and inform the user.
+            foreach (string item in myQueue)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+
+
+
+        //DICTIONARY
+
+        static Dictionary<string, int> myDictionary = new Dictionary<string, int>();
+
+        static void addToDictionary()
+        {
+            //prompts the user to enter a string and then inserts the input into the data structure.
+            Console.WriteLine("Enter a string key");
+            string userInput = Console.ReadLine();
+            Console.WriteLine("Enter a int value");
+            string userIntString = Console.ReadLine();
+            int userInt = Convert.ToInt32(userIntString);
+            myDictionary.Add(userInput, userInt); //is this right?
+        }
+        static void addHugeToDictionary()
+        {
+            //first clears the data structure and 
+            //then generate 2,000 items in the data structure with the value of “New Entry” concatenated with the number. 
+            //For example, New Entry 1, New Entry 2, New Entry 3. For the dictionary, the key will be the generated string ("New Entry 2") and the value will be the current number (2).
+            myDictionary.Clear();
+            for (int i = 1; i < 2001; i++)
+            { 
+                myDictionary.Add("New Entry " + i.ToString(), i);
+            }
+        }
+        static void displayDictionary()
+        {
+            //display the contents of the data structure. You must use the foreach loop when displaying the data. Handle any errors and inform the user.
+            foreach (KeyValuePair<string, int> item in myDictionary)
+            {
+                Console.WriteLine("Key: " + item.Key + " Value: " + item.Value);
+            }
+
+        }
+
+
+
+
+            static void Main(string[] args)
         {
             int userInt;
             bool keepGoing = true;
@@ -44,10 +150,13 @@ namespace ConsoleApp1
                         switch (userInt)
                         {
                             case 1:
+                                addToStack();
                                 break;
                             case 2:
+                                addHugeToStack();
                                 break;
                             case 3:
+                                displayStack();
                                 break;
                             case 4:
                                 break;
@@ -80,10 +189,13 @@ namespace ConsoleApp1
                         switch (userInt)
                         {
                             case 1:
+                                addToQueue();
                                 break;
                             case 2:
+                                addHugeToQueue();
                                 break;
                             case 3:
+                                displayQueue();
                                 break;
                             case 4:
                                 break;
@@ -115,10 +227,13 @@ namespace ConsoleApp1
                         switch (userInt)
                         {
                             case 1:
+                                addToDictionary();
                                 break;
                             case 2:
+                                addHugeToDictionary();
                                 break;
                             case 3:
+                                displayDictionary();
                                 break;
                             case 4:
                                 break;
