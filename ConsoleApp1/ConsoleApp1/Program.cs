d618bd000f7804001c02c15387b03eb21813e40c
Tyler Bradshaw <bradshaw.tyler@gmail.com>
2017-10-02 14:16:01 -0600

    Most of the backbone should be done, it checks for a valid number and keeps going until the user chooses "4" for exit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,63 +11,135 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             int userInt;
+            bool keepGoing = true;
 
-
-            Console.WriteLine("1. Stack");
-            Console.WriteLine("2. Queue");
-            Console.WriteLine("3. Dictionary");
-            Console.WriteLine("4. Exit");
-            Console.WriteLine();
-
-            userInt = int.Parse(Console.ReadLine());
-            switch (userInt)
+            while (keepGoing == true)
             {
-                case 1:
-                    Console.WriteLine("1.Add one time to Stack");
-                    Console.WriteLine("2. Add Huge List of Items to Stack");
-                    Console.WriteLine("3. Display Stack");
-                    Console.WriteLine("4. Delete from Stack");
-                    Console.WriteLine("5. Clear Stack");
-                    Console.WriteLine("6. Search Stack");
-                    Console.WriteLine("7. Return to Main Menu");
+                do
+                {
+                    Console.WriteLine("1. Stack");
+                    Console.WriteLine("2. Queue");
+                    Console.WriteLine("3. Dictionary");
+                    Console.WriteLine("4. Exit");
                     Console.WriteLine();
+                }
+                while (!int.TryParse(Console.ReadLine(), out userInt));
 
-                    int userInput;
+                switch (userInt)
+                {
+                    case 1:
+                        do
+                        {
+                            Console.WriteLine("1. Add one time to Stack");
+                            Console.WriteLine("2. Add Huge List of Items to Stack");
+                            Console.WriteLine("3. Display Stack");
+                            Console.WriteLine("4. Delete from Stack");
+                            Console.WriteLine("5. Clear Stack");
+                            Console.WriteLine("6. Search Stack");
+                            Console.WriteLine("7. Return to Main Menu");
+                            Console.WriteLine();
+                        }
+                        while (!int.TryParse(Console.ReadLine(), out userInt));
 
-                    userInput = int.Parse(Console.ReadLine());
-                    switch (userInt\put)
-                        case 1:
-                    break;
-                        
-                case 2:
-                    Console.WriteLine("1. Add one time to Queue");
-                    Console.WriteLine("2. Add Huge List of Items to Queue");
-                    Console.WriteLine("3. Display Queue");
-                    Console.WriteLine("4. Delete from Queue");
-                    Console.WriteLine("5. Clear Queue");
-                    Console.WriteLine("6. Search Queue");
-                    Console.WriteLine("7. Return to Main Menu");
-                    Console.WriteLine();
-                    break;
-                case 3:
-                    Console.WriteLine("1. Add one item to Dictionary");
-                    Console.WriteLine("2. Add Huge List of Items to Dictionary");
-                    Console.WriteLine("3. Display Dictionary");
-                    Console.WriteLine("4. Delete from Dictionary");
-                    Console.WriteLine("5. Clear Dictionary");
-                    Console.WriteLine("6. Search Dictionary");
-                    Console.WriteLine("7. Return to Main Menu");
-                    Console.WriteLine();
-                    break;
-                case 4:
-                    break;
-                default:
-                    break;
+                        switch (userInt)
+                        {
+                            case 1:
+                                break;
+                            case 2:
+                                break;
+                            case 3:
+                                break;
+                            case 4:
+                                break;
+                            case 5:
+                                break;
+                            case 6:
+                                break;
+                            case 7:
+                                break;
+                            default:
+                                break;
+                        }
 
-               //comment
-               //new comment just for Eric
+                        break;
+
+                    case 2:
+                        do
+                        {
+                            Console.WriteLine("1. Add one time to Queue");
+                            Console.WriteLine("2. Add Huge List of Items to Queue");
+                            Console.WriteLine("3. Display Queue");
+                            Console.WriteLine("4. Delete from Queue");
+                            Console.WriteLine("5. Clear Queue");
+                            Console.WriteLine("6. Search Queue");
+                            Console.WriteLine("7. Return to Main Menu");
+                            Console.WriteLine();
+                        }
+                        while (!int.TryParse(Console.ReadLine(), out userInt));
+
+                        switch (userInt)
+                        {
+                            case 1:
+                                break;
+                            case 2:
+                                break;
+                            case 3:
+                                break;
+                            case 4:
+                                break;
+                            case 5:
+                                break;
+                            case 6:
+                                break;
+                            case 7:
+                                break;
+                            default:
+                                break;
+                        }
+
+                        break;
+                    case 3:
+                        do
+                        {
+                            Console.WriteLine("1. Add one item to Dictionary");
+                            Console.WriteLine("2. Add Huge List of Items to Dictionary");
+                            Console.WriteLine("3. Display Dictionary");
+                            Console.WriteLine("4. Delete from Dictionary");
+                            Console.WriteLine("5. Clear Dictionary");
+                            Console.WriteLine("6. Search Dictionary");
+                            Console.WriteLine("7. Return to Main Menu");
+                            Console.WriteLine();
+                        }
+                        while (!int.TryParse(Console.ReadLine(), out userInt));
+
+                        switch (userInt)
+                        {
+                            case 1:
+                                break;
+                            case 2:
+                                break;
+                            case 3:
+                                break;
+                            case 4:
+                                break;
+                            case 5:
+                                break;
+                            case 6:
+                                break;
+                            case 7:
+                                break;
+                            default:
+                                break;
+                        }
+
+                        break;
+                    case 4:
+                        keepGoing = false;
+                        break;
+                    default:
+                        break;
+                }
             }
-
-}
+        }
     }
 }
